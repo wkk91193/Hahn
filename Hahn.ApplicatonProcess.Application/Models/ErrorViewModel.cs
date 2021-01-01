91299e5ee98822b492b29d8f3a23e62c68c4a481
K2 <kumaranath.fernando@thitsaworks.com>
2021-01-01 18:59:31 +0700

    Committing initial project setup

---
+++ Hahn.ApplicatonProcess.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hahn.ApplicatonProcess.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
